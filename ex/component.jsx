c6f7bccdc99094c046aa84d43f2935340cd1262c
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-24 15:19:52 -0300

    feat: create MyComponent

---
+++ ex/component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Olá React</h1>
+      <p>Esse é um componente React</p>
+    </div>
+  );
+}
--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import myComponent from "./component.jsx";
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById("app")); 
+ReactDOM.render(<myComponent/>, document.getElementById("app")); 
