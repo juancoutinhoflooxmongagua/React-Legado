e2139675cc9ac31c3f4c5e53453d4808271d6b4b
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-24 15:20:48 -0300

    feat: change component for arrow component

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,10 +1,3 @@
 import React from "react";
 
-function MyComponent() {
-  return (
-    <div>
-      <h1>Olá React</h1>
-      <p>Esse é um componente React</p>
-    </div>
-  );
-}
+export default () => <h1>Primeiro Componente</h1>
